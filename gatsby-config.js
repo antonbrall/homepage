4040501c367af429aede0b5e67d2bb33b874b715
Anton Brall <info@antonbrall.de>
2024-02-20 15:29:22 +0100

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
         {
           resolve: "gatsby-remark-images",
           options: {
-            maxWidth: 1200,
+            maxWidth: 2000,
           },
         },
       ],
